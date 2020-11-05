2d0b06194a727a71cfb6d00202f3b23d5055d3a8
khoapmd <71309224+khoapmd@users.noreply.github.com>
2020-11-05 16:35:55 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -203,6 +203,7 @@ namespace FabBots {
 
     // wait for certain response from Nano
     function waitResponse() {
+        if(blynk_connected == true) return
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
             let serial_str: string = ""
             let time: number = input.runningTime()
